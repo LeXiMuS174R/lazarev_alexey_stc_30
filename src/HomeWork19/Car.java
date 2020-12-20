246edde0b94fa40d20cfdb1842e73d5d193bf579
Alexey Lazarev <flome777@gmail.com>
2020-12-20 23:07:20 +0500

    Add HomeWork19
    Sorry for copy cars.txt xD

---
+++ src/HomeWork19/Car.java
@@ -0,0 +1,80 @@
+package HomeWork19;
+
+import java.util.Objects;
+
+public class Car implements Comparable<Car> {
+
+    private String numberCar;
+
+    private String model;
+
+    private String color;
+
+    private Long run;
+
+    private Long price;
+
+    public Car(String numberCar, String model, String color, Long run, Long price) {
+        this.numberCar = numberCar;
+        this.model = model;
+        this.color = color;
+        this.run = run;
+        this.price = price;
+    }
+
+    public String getNumberCar() {
+        return numberCar;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public Long getRun() {
+        return run;
+    }
+
+    public Long getPrice() {
+        return price;
+    }
+
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Car)) return false;
+        Car car = (Car) o;
+        return Objects.equals(getNumberCar(), car.getNumberCar()) &&
+                Objects.equals(getModel(), car.getModel()) &&
+                Objects.equals(getColor(), car.getColor()) &&
+                Objects.equals(getRun(), car.getRun()) &&
+                Objects.equals(getPrice(), car.getPrice());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getNumberCar(), getModel(), getColor(), getRun(), getPrice());
+    }
+
+
+    @Override
+    public int compareTo(Car o) {
+        return this.model.hashCode() - o.getModel().hashCode();
+    }
+
+    @Override
+    public String toString() {
+        return "Car{" +
+                "numberCar='" + numberCar + '\'' +
+                ", model='" + model + '\'' +
+                ", color='" + color + '\'' +
+                ", run=" + run +
+                ", price=" + price +
+                '}';
+    }
+}
+
---
+++ src/HomeWork19/Main.java
@@ -0,0 +1,60 @@
+package HomeWork19;
+
+import java.io.IOException;
+import java.nio.file.Files;
+
+import java.nio.file.Paths;
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class Main {
+    public static void main(String[] args) {
+
+        try {
+            /**
+             * #1 Номера всех автомобилей, имеющих черный цвет или нулевой пробег.
+             */
+            Files.lines(Paths.get("src/cars.txt"))
+                    .map(line -> line.split(" "))
+                    .map(array -> new Car(array[0], array[1], array[2], Long.parseLong(array[3]), Long.parseLong(array[4])))
+                    .filter(car -> car.getColor().equals("Black") || car.getRun().equals(0L))
+                    .forEach(car -> System.out.println(car.getNumberCar()));
+            /**
+             * #2 Количество уникальных моделей в ценовом диапазоне от 700 до 800 тыс.
+             */
+            int size = Files.lines(Paths.get("src/cars.txt"))
+                    .map(line -> line.split(" "))
+                    .map(array -> new Car(array[0], array[1], array[2], Long.parseLong(array[3]), Long.parseLong(array[4])))
+                    .map(Car::getModel)
+                    .collect(Collectors.toSet()).size();
+            System.out.println(size);
+            /**
+             * #3 Вывести цвет автомобиля с минимальной стоимостью.
+            */
+
+            String color = Files.lines(Paths.get("src/cars.txt"))
+                    .map(line -> line.split(" "))
+                    .map(array -> new Car(array[0], array[1], array[2], Long.parseLong(array[3]), Long.parseLong(array[4])))
+                    .min(Comparator.comparing(Car::getPrice))
+                    .get()
+                    .getColor();
+            System.out.println(color);
+            /**
+             * #4 СредняяЯ стоимость Camry.
+             */
+
+            OptionalDouble se = Files.lines(Paths.get("src/cars.txt"))
+                    .map(line -> line.split(" "))
+                    .map(array -> new Car(array[0], array[1], array[2], Long.parseLong(array[3]), Long.parseLong(array[4])))
+                    .filter(car -> car.getModel().equals("TOYOTA_CAMRY"))
+                    .mapToLong(Car::getPrice)
+                    .average();
+            System.out.println(se);
+
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+}
